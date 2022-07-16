a9686fb7b6d9ae9404b71b716de72b904e6fb0ed
Kenyon Allan <48738848+kenyon-allan@users.noreply.github.com>
2022-07-16 14:56:30 -0400

    Update FruitShape.ts

--- dice-grower/src/types/FruitShape.ts
+++ dice-grower/src/types/FruitShape.ts
@@ -1,28 +1,31 @@
 export class FruitShape {
+    
     dieShape:number;
-    constructor(dieShape:number) {
-        this.dieShape = numberofsides()
+
+    constructor() {
+        this.dieShape = this.numberofsides()
     }
-    function numberofsides() {
+    private numberofsides() {
         // pick between d4 (0), d6(1), d8(2), d10(3), d12(4), d20(5)
         let customSides = Math.floor(Math.random() * (6))
-        if (customSides == 0) {
-            let dieShape = 4;
+        let dieShape = 4;
+        if (customSides === 0) {
+            dieShape = 4;
         }
-        else if (customSides == 1) {
-            let dieShape = 6;
+        else if (customSides === 1) {
+            dieShape = 6;
         }
-        else if (customSides == 2) {
-            let dieShape = 8;
+        else if (customSides === 2) {
+            dieShape = 8;
         }
-        else if (customSides == 3) {
-            let dieShape = 10;
+        else if (customSides === 3) {
+            dieShape = 10;
         }
-        else if (customSides == 4) {
-            let dieShape = 12;
+        else if (customSides === 4) {
+            dieShape = 12;
         }
-        else if (customSides == 5) {
-            let dieShape = 20;
+        else if (customSides === 5) {
+            dieShape = 20;
         }
 
         return dieShape;
