45456328ee546ad1f54b673d3bdb3e3c71dedd3b
Michael Madden <michaeltm98@gmail.com>
2022-07-17 09:24:36 -0400

    comment code fix error

--- dice-grower/src/models/CustomerModel.ts
+++ dice-grower/src/models/CustomerModel.ts
@@ -5,9 +5,11 @@ export default class CustomerModel {
     constructor(desiredFruit: FruitModel) {
         this.desiredFruit = desiredFruit;
     //use some sort of RNG depending on how we store/order fruits
-        function customOrder() {
-            // pick between d4 (0), d6(1), d8(2), d10(3), d12(4), d20(5)
-            desiredFruit.dieSize = Math.floor(Math.random() * (6))
     }
-    }
+
+    // function customOrder() {
+    //     // pick between d4 (0), d6(1), d8(2), d10(3), d12(4), d20(5)
+    //     return new FruitModel()
+    //     desiredFruit.dieSize = Math.floor(Math.random() * (6))
+    // }
 }
